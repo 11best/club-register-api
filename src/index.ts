d6ce47cad448b3c7cc0f6ef2152aa8988d772564
11best <asama_b@outlook.com>
2024-09-01 19:09:27 +0700

    init prisma and get all from teachers

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { Elysia } from "elysia";
+import { PrismaClient } from "@prisma/client";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+const prisma = new PrismaClient();
+
+async function getTeachers() {
+  try {
+    return await prisma.teachers.findMany();
+  } catch (e: unknown) {
+    console.error(`Error getting posts: ${e}`);
+  }
+}
+
+app.get("/", () => "Hello Elysia");
+app.get("/teachers", () => getTeachers());
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
