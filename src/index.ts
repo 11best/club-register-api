8d71f8eeebdf37ecc329fbc5ad15c77069a9983d
11best <asama_b@outlook.com>
2024-09-05 21:25:03 +0700

    add onError handler and create verify-id and register

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia, t } from "elysia";
+import { Elysia, NotFoundError, t } from "elysia";
 import {
   getTeachers,
   getTeacher,
@@ -32,9 +32,45 @@ const teachers = new Elysia({ prefix: "/teacher" })
         .delete("/:id", ({ params: { id } }) => deleteTeacher(id))
   );
 
+async function verifyId(id: string) {
+  const idMatched = await getTeacher(id);
+  if (!idMatched) {
+    throw new NotFoundError();
+  }
+  return { isFound: !!idMatched };
+}
+
 const app = new Elysia()
+  .onError(({ code, error, set }) => {
+    if (code === "NOT_FOUND") {
+      set.status = 404;
+      return "Not Found";
+    }
+  })
   .use(teachers)
   .get("/", () => "Hello Elysia")
+  .post("/verify-id", ({ body: { id } }) => verifyId(id), {
+    body: t.Object({
+      id: t.String(),
+    }),
+  })
+  .post(
+    "/register",
+    async ({ body }) => {
+      let userData: any = body;
+      userData.password = await Bun.password.hash(userData.password, {
+        algorithm: "bcrypt",
+        cost: 4,
+      });
+      return userData;
+    },
+    {
+      body: t.Object({
+        id: t.String(),
+        password: t.String(),
+      }),
+    }
+  )
   .listen(3000);
 
 console.log(
