08147152b28071b942e1b9b578d9197fca3519e1
11best <asama_b@outlook.com>
2024-09-08 22:56:59 +0700

    update env

--- src/loginUser/route.ts
+++ src/loginUser/route.ts
@@ -5,9 +5,28 @@ import {
   registerLoginUser,
   verifyId,
 } from "./handler";
+import jwt from "@elysiajs/jwt";
 
 export const user = new Elysia()
-  .get("/login-user", () => getLoginUsers())
+  .use(
+    jwt({
+      name: "jwt",
+      secret: process.env.JWT_SECRET!,
+    })
+  )
+  .get(
+    "/login-user",
+    async ({ jwt, set, cookie: { auth } }) => {
+      const profile = await jwt.verify(auth.value);
+      if (!profile) {
+        set.status = 401;
+        return "Unauthorized";
+      }
+
+      return `Hello ${profile.id}`;
+    }
+    // () => getLoginUsers()
+  )
   .post("/verify-id", ({ body: { id } }) => verifyId(id), {
     body: t.Object({
       id: t.Number(),
@@ -23,5 +42,18 @@ export const user = new Elysia()
     (app) =>
       app
         .post("/register", async ({ body }) => registerLoginUser(body))
-        .post("/login", async ({ body }) => loginUser(body))
+        .post(
+          "/login",
+          async ({ jwt, cookie: { auth }, body }) => {
+            auth.set({
+              value: await jwt.sign({ id: body.id }),
+              httpOnly: true,
+              maxAge: 7 * 86400,
+            });
+
+            return `Sign in as ${auth.value} ${auth}`;
+          }
+
+          // async ({ body }) => loginUser(body)
+        )
   );
