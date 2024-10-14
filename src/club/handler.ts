dfeb5341bc60e27843bf12ddf81d570d2fd035c6
11best <asama_b@outlook.com>
2024-10-14 22:29:47 +0700

    CRUD for club

--- src/club/handler.ts
+++ src/club/handler.ts
@@ -1,11 +1,61 @@
 import { PrismaClient } from "@prisma/client";
+import { NotFoundError } from "elysia";
+
+interface ClubRequest {
+  id?: number;
+  name: string;
+}
 
 const prisma = new PrismaClient();
 
 export async function getClubs() {
   try {
-    return await prisma.user.findMany();
+    return await prisma.club.findMany();
   } catch (e: unknown) {
     console.error(`Error getting clubs: ${e}`);
   }
+}
+
+export async function getClub(id: number) {
+  const club = await prisma.club.findUnique({
+    where: { id: id },
+  });
+  if (!club) {
+    throw new NotFoundError();
+  }
+  return club;
+}
+
+export async function createClub(req: ClubRequest) {
+  try {
+    return await prisma.club.create({
+      data: {
+        name: req.name,
+      },
+    });
+  } catch (e: unknown) {
+    console.error(`Error creating club: ${e}`);
+  }
+}
+
+export async function updateClub(req: ClubRequest) {
+  try {
+    return await prisma.club.update({
+      where: {
+        id: req.id,
+      },
+      data: {
+        name: req.name,
+      },
+    });
+  } catch (e: unknown) {
+    console.error(`Error updating club: ${e}`);
+  }
+}
+
+export async function deleteClub(id: number) {
+  await prisma.club.delete({
+    where: { id: id },
+  });
+  return { message: `club id: ${id} delete successful!` };
 }
--- src/club/route.ts
+++ src/club/route.ts
@@ -1,4 +1,31 @@
 import { Elysia, t } from "elysia";
-import { getClubs } from "./handler";
+import {
+  createClub,
+  deleteClub,
+  getClub,
+  getClubs,
+  updateClub,
+} from "./handler";
 
-export const club = new Elysia({ prefix: "/club" }).get("/", () => getClubs());
+export const club = new Elysia({ prefix: "/club" })
+  .get("/", () => getClubs())
+  .post("/", ({ body: { name } }) => createClub({ name }), {
+    body: t.Object({
+      name: t.String(),
+    }),
+  })
+  .put("/", ({ body }) => updateClub(body), {
+    body: t.Object({
+      id: t.Number(),
+      name: t.String(),
+    }),
+  })
+  .guard(
+    {
+      params: t.Object({ id: t.Number() }),
+    },
+    (app) =>
+      app
+        .get("/:id", ({ params: { id } }) => getClub(id))
+        .delete("/:id", ({ params: { id } }) => deleteClub(id))
+  );
